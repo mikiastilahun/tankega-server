e948babe0fee4247a182ce5188c897555f8c5a01
Mike Abebe <mikiastilahun@gmail.com>
2019-07-25 13:50:19 +0300

    added test url for checking weather the server is working

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const io = require('socket.io')(server);
 const p2p = require('socket.io-p2p-server').Server;
 io.use(p2p);
 
-
+app.get('/', (req,res)=>{
+    res.json("the ec2 is working")
+})
 
 server.listen(3000, () => {
     console.log("the app is running")
